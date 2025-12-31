5b3c24c769ac9f114b04a2cfb586c3aa252a8b6e
edcalderon <edwardca12@hotmail.com>
2025-12-31 17:48:03 -0500

    fix(ci): update github actions to latest versions to resolve deprecation failures

--- apps/landing/out/static/chunks/webpack.js
+++ apps/landing/out/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ec673f145f947f4f"; }
+/******/ 		__webpack_require__.h = function() { return "0e8b9d95a90663ab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
